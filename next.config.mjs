753d7ec6dad9393a305cf621539bf796f31d87e5
Escobar <roshanjr464@gmail.com>
2026-02-09 14:40:06 +0545

    config changes

--- next.config.mjs
+++ next.config.mjs
@@ -3,9 +3,10 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  output: "export",
   images: {
     unoptimized: true,
   },
-}
+};
 
-export default nextConfig
+export default nextConfig;
